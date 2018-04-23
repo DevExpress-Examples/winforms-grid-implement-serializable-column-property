ce31a57482494a6bba7fcf51456340ba9fd1f246
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Q242361/Form1.Designer.cs
@@ -0,0 +1,173 @@
+namespace Q242361 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.myGridControl1 = new DXSample.MyGridControl();
+            this.categoriesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.nwindDataSet = new Q242361.nwindDataSet();
+            this.myGridView1 = new DXSample.MyGridView();
+            this.colCategoryID = new DXSample.MyGridColumn();
+            this.colCategoryName = new DXSample.MyGridColumn();
+            this.colDescription = new DXSample.MyGridColumn();
+            this.categoriesTableAdapter = new Q242361.nwindDataSetTableAdapters.CategoriesTableAdapter();
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.myGridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.categoriesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nwindDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // myGridControl1
+            // 
+            this.myGridControl1.DataSource = this.categoriesBindingSource;
+            this.myGridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.myGridControl1.Location = new System.Drawing.Point(0, 0);
+            this.myGridControl1.MainView = this.myGridView1;
+            this.myGridControl1.Name = "myGridControl1";
+            this.myGridControl1.Size = new System.Drawing.Size(401, 234);
+            this.myGridControl1.TabIndex = 0;
+            this.myGridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.myGridView1});
+            // 
+            // categoriesBindingSource
+            // 
+            this.categoriesBindingSource.DataMember = "Categories";
+            this.categoriesBindingSource.DataSource = this.nwindDataSet;
+            // 
+            // nwindDataSet
+            // 
+            this.nwindDataSet.DataSetName = "nwindDataSet";
+            this.nwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // myGridView1
+            // 
+            this.myGridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colCategoryID,
+            this.colCategoryName,
+            this.colDescription});
+            this.myGridView1.GridControl = this.myGridControl1;
+            this.myGridView1.Name = "myGridView1";
+            // 
+            // colCategoryID
+            // 
+            this.colCategoryID.Caption = "ID";
+            this.colCategoryID.FieldName = "CategoryID";
+            this.colCategoryID.Name = "colCategoryID";
+            this.colCategoryID.OldVisibleIndex = 0;
+            this.colCategoryID.Visible = true;
+            this.colCategoryID.VisibleIndex = 0;
+            // 
+            // colCategoryName
+            // 
+            this.colCategoryName.Caption = "Name";
+            this.colCategoryName.FieldName = "CategoryName";
+            this.colCategoryName.Name = "colCategoryName";
+            this.colCategoryName.OldVisibleIndex = 1;
+            this.colCategoryName.Visible = true;
+            this.colCategoryName.VisibleIndex = 1;
+            // 
+            // colDescription
+            // 
+            this.colDescription.Caption = "Description";
+            this.colDescription.FieldName = "Description";
+            this.colDescription.Name = "colDescription";
+            this.colDescription.OldVisibleIndex = 2;
+            this.colDescription.Visible = true;
+            this.colDescription.VisibleIndex = 2;
+            // 
+            // categoriesTableAdapter
+            // 
+            this.categoriesTableAdapter.ClearBeforeFill = true;
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.simpleButton2);
+            this.panelControl1.Controls.Add(this.simpleButton1);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelControl1.Location = new System.Drawing.Point(0, 234);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(401, 39);
+            this.panelControl1.TabIndex = 1;
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(93, 6);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(87, 23);
+            this.simpleButton2.TabIndex = 1;
+            this.simpleButton2.Text = "Restore Layout";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(12, 6);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "Save Layout";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 273);
+            this.Controls.Add(this.myGridControl1);
+            this.Controls.Add(this.panelControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.myGridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.categoriesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nwindDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DXSample.MyGridControl myGridControl1;
+        private nwindDataSet nwindDataSet;
+        private System.Windows.Forms.BindingSource categoriesBindingSource;
+        private Q242361.nwindDataSetTableAdapters.CategoriesTableAdapter categoriesTableAdapter;
+        private DXSample.MyGridView myGridView1;
+        private DXSample.MyGridColumn colCategoryID;
+        private DXSample.MyGridColumn colCategoryName;
+        private DXSample.MyGridColumn colDescription;
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+
+
+    }
+}
+
