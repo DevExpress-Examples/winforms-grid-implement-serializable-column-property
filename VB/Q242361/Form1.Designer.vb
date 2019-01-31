286138f5b9d01513b224e5799243a8efc4b1f8c4
DevExpressExampleBot <fakeEmail>
2019-01-31 15:12:58 +0300

    Source auto update [skip ci]

--- VB/Q242361/Form1.Designer.vb
+++ VB/Q242361/Form1.Designer.vb
@@ -1,145 +1,140 @@
-Imports Microsoft.VisualBasic
-Imports System
-Namespace Q242361
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+﻿Namespace Q242361
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#Region "Windows Form Designer generated code"
+        #Region "Windows Form Designer generated code"
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.myGridControl1 = New DXSample.MyGridControl()
-			Me.myGridView1 = New DXSample.MyGridView()
-			Me.colCategoryID = New DXSample.MyGridColumn()
-			Me.colCategoryName = New DXSample.MyGridColumn()
-			Me.colDescription = New DXSample.MyGridColumn()
-			Me.panelControl1 = New DevExpress.XtraEditors.PanelControl()
-			Me.simpleButton2 = New DevExpress.XtraEditors.SimpleButton()
-			Me.simpleButton1 = New DevExpress.XtraEditors.SimpleButton()
-			CType(Me.myGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.myGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.panelControl1.SuspendLayout()
-			Me.SuspendLayout()
-			' 
-			' myGridControl1
-			' 
-			Me.myGridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.myGridControl1.Location = New System.Drawing.Point(0, 0)
-			Me.myGridControl1.MainView = Me.myGridView1
-			Me.myGridControl1.Name = "myGridControl1"
-			Me.myGridControl1.Size = New System.Drawing.Size(401, 234)
-			Me.myGridControl1.TabIndex = 0
-			Me.myGridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.myGridView1})
-			' 
-			' myGridView1
-			' 
-			Me.myGridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colCategoryID, Me.colCategoryName, Me.colDescription})
-			Me.myGridView1.GridControl = Me.myGridControl1
-			Me.myGridView1.Name = "myGridView1"
-			' 
-			' colCategoryID
-			' 
-			Me.colCategoryID.Caption = "ID"
-			Me.colCategoryID.FieldName = "ID"
-			Me.colCategoryID.Name = "colCategoryID"
-			Me.colCategoryID.OldVisibleIndex = 0
-			Me.colCategoryID.Visible = True
-			Me.colCategoryID.VisibleIndex = 0
-			' 
-			' colCategoryName
-			' 
-			Me.colCategoryName.Caption = "Name"
-			Me.colCategoryName.FieldName = "Name"
-			Me.colCategoryName.Name = "colCategoryName"
-			Me.colCategoryName.OldVisibleIndex = 1
-			Me.colCategoryName.Visible = True
-			Me.colCategoryName.VisibleIndex = 1
-			' 
-			' colDescription
-			' 
-			Me.colDescription.Caption = "Description"
-			Me.colDescription.FieldName = "Description"
-			Me.colDescription.Name = "colDescription"
-			Me.colDescription.OldVisibleIndex = 2
-			Me.colDescription.Visible = True
-			Me.colDescription.VisibleIndex = 2
-			' 
-			' panelControl1
-			' 
-			Me.panelControl1.Controls.Add(Me.simpleButton2)
-			Me.panelControl1.Controls.Add(Me.simpleButton1)
-			Me.panelControl1.Dock = System.Windows.Forms.DockStyle.Bottom
-			Me.panelControl1.Location = New System.Drawing.Point(0, 234)
-			Me.panelControl1.Name = "panelControl1"
-			Me.panelControl1.Size = New System.Drawing.Size(401, 39)
-			Me.panelControl1.TabIndex = 1
-			' 
-			' simpleButton2
-			' 
-			Me.simpleButton2.Location = New System.Drawing.Point(93, 6)
-			Me.simpleButton2.Name = "simpleButton2"
-			Me.simpleButton2.Size = New System.Drawing.Size(87, 23)
-			Me.simpleButton2.TabIndex = 1
-			Me.simpleButton2.Text = "Restore Layout"
-'			Me.simpleButton2.Click += New System.EventHandler(Me.simpleButton2_Click);
-			' 
-			' simpleButton1
-			' 
-			Me.simpleButton1.Location = New System.Drawing.Point(12, 6)
-			Me.simpleButton1.Name = "simpleButton1"
-			Me.simpleButton1.Size = New System.Drawing.Size(75, 23)
-			Me.simpleButton1.TabIndex = 0
-			Me.simpleButton1.Text = "Save Layout"
-'			Me.simpleButton1.Click += New System.EventHandler(Me.simpleButton1_Click);
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(401, 273)
-			Me.Controls.Add(Me.myGridControl1)
-			Me.Controls.Add(Me.panelControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-'			Me.Load += New System.EventHandler(Me.Form1_Load);
-			CType(Me.myGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.myGridView1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.panelControl1.ResumeLayout(False)
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.myGridControl1 = New DXSample.MyGridControl()
+            Me.myGridView1 = New DXSample.MyGridView()
+            Me.colCategoryID = New DXSample.MyGridColumn()
+            Me.colCategoryName = New DXSample.MyGridColumn()
+            Me.colDescription = New DXSample.MyGridColumn()
+            Me.panelControl1 = New DevExpress.XtraEditors.PanelControl()
+            Me.simpleButton2 = New DevExpress.XtraEditors.SimpleButton()
+            Me.simpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+            CType(Me.myGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.myGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.panelControl1.SuspendLayout()
+            Me.SuspendLayout()
+            ' 
+            ' myGridControl1
+            ' 
+            Me.myGridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.myGridControl1.Location = New System.Drawing.Point(0, 0)
+            Me.myGridControl1.MainView = Me.myGridView1
+            Me.myGridControl1.Name = "myGridControl1"
+            Me.myGridControl1.Size = New System.Drawing.Size(401, 234)
+            Me.myGridControl1.TabIndex = 0
+            Me.myGridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.myGridView1})
+            ' 
+            ' myGridView1
+            ' 
+            Me.myGridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colCategoryID, Me.colCategoryName, Me.colDescription})
+            Me.myGridView1.GridControl = Me.myGridControl1
+            Me.myGridView1.Name = "myGridView1"
+            ' 
+            ' colCategoryID
+            ' 
+            Me.colCategoryID.Caption = "ID"
+            Me.colCategoryID.FieldName = "ID"
+            Me.colCategoryID.Name = "colCategoryID"
+            Me.colCategoryID.OldVisibleIndex = 0
+            Me.colCategoryID.Visible = True
+            Me.colCategoryID.VisibleIndex = 0
+            ' 
+            ' colCategoryName
+            ' 
+            Me.colCategoryName.Caption = "Name"
+            Me.colCategoryName.FieldName = "Name"
+            Me.colCategoryName.Name = "colCategoryName"
+            Me.colCategoryName.OldVisibleIndex = 1
+            Me.colCategoryName.Visible = True
+            Me.colCategoryName.VisibleIndex = 1
+            ' 
+            ' colDescription
+            ' 
+            Me.colDescription.Caption = "Description"
+            Me.colDescription.FieldName = "Description"
+            Me.colDescription.Name = "colDescription"
+            Me.colDescription.OldVisibleIndex = 2
+            Me.colDescription.Visible = True
+            Me.colDescription.VisibleIndex = 2
+            ' 
+            ' panelControl1
+            ' 
+            Me.panelControl1.Controls.Add(Me.simpleButton2)
+            Me.panelControl1.Controls.Add(Me.simpleButton1)
+            Me.panelControl1.Dock = System.Windows.Forms.DockStyle.Bottom
+            Me.panelControl1.Location = New System.Drawing.Point(0, 234)
+            Me.panelControl1.Name = "panelControl1"
+            Me.panelControl1.Size = New System.Drawing.Size(401, 39)
+            Me.panelControl1.TabIndex = 1
+            ' 
+            ' simpleButton2
+            ' 
+            Me.simpleButton2.Location = New System.Drawing.Point(93, 6)
+            Me.simpleButton2.Name = "simpleButton2"
+            Me.simpleButton2.Size = New System.Drawing.Size(87, 23)
+            Me.simpleButton2.TabIndex = 1
+            Me.simpleButton2.Text = "Restore Layout"
+            ' 
+            ' simpleButton1
+            ' 
+            Me.simpleButton1.Location = New System.Drawing.Point(12, 6)
+            Me.simpleButton1.Name = "simpleButton1"
+            Me.simpleButton1.Size = New System.Drawing.Size(75, 23)
+            Me.simpleButton1.TabIndex = 0
+            Me.simpleButton1.Text = "Save Layout"
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(401, 273)
+            Me.Controls.Add(Me.myGridControl1)
+            Me.Controls.Add(Me.panelControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType(Me.myGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.myGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.panelControl1.ResumeLayout(False)
+            Me.ResumeLayout(False)
 
-		End Sub
+        End Sub
 
-		#End Region
+        #End Region
 
-		Private myGridControl1 As DXSample.MyGridControl
-		Private myGridView1 As DXSample.MyGridView
-		Private colCategoryID As DXSample.MyGridColumn
-		Private colCategoryName As DXSample.MyGridColumn
-		Private colDescription As DXSample.MyGridColumn
-		Private panelControl1 As DevExpress.XtraEditors.PanelControl
-		Private WithEvents simpleButton2 As DevExpress.XtraEditors.SimpleButton
-		Private WithEvents simpleButton1 As DevExpress.XtraEditors.SimpleButton
+        Private myGridControl1 As DXSample.MyGridControl
+        Private myGridView1 As DXSample.MyGridView
+        Private colCategoryID As DXSample.MyGridColumn
+        Private colCategoryName As DXSample.MyGridColumn
+        Private colDescription As DXSample.MyGridColumn
+        Private panelControl1 As DevExpress.XtraEditors.PanelControl
+        Private WithEvents simpleButton2 As DevExpress.XtraEditors.SimpleButton
+        Private WithEvents simpleButton1 As DevExpress.XtraEditors.SimpleButton
 
 
-	End Class
+    End Class
 End Namespace
 
--- VB/Q242361/Grid.vb
+++ VB/Q242361/Grid.vb
@@ -1,5 +1,4 @@
-Imports Microsoft.VisualBasic
-Imports DevExpress.XtraGrid
+﻿Imports DevExpress.XtraGrid
 Imports DevExpress.XtraGrid.Views.Grid
 Imports DevExpress.XtraGrid.Registrator
 Imports DevExpress.XtraGrid.Views.Base
@@ -8,117 +7,123 @@ Imports System.ComponentModel
 Imports DevExpress.Utils.Serializing
 
 Namespace DXSample
-	Public Class MyGridControl
-		Inherits GridControl
-		Public Sub New()
-			MyBase.New()
-		End Sub
+    Public Class MyGridControl
+        Inherits GridControl
 
-		Protected Overrides Sub RegisterAvailableViewsCore(ByVal collection As InfoCollection)
-			MyBase.RegisterAvailableViewsCore(collection)
-			collection.Add(New MyGridViewInfoRegistrator())
-		End Sub
-	End Class
+        Public Sub New()
+            MyBase.New()
+        End Sub
 
-	Public Class MyGridView
-		Inherits GridView
-		Public Sub New()
-			MyBase.New()
-		End Sub
-		Public Sub New(ByVal grid As GridControl)
-			MyBase.New(grid)
-		End Sub
+        Protected Overrides Sub RegisterAvailableViewsCore(ByVal collection As InfoCollection)
+            MyBase.RegisterAvailableViewsCore(collection)
+            collection.Add(New MyGridViewInfoRegistrator())
+        End Sub
+    End Class
 
-		Friend Const MyGridViewName As String = "MyGridView"
+    Public Class MyGridView
+        Inherits GridView
 
-		Protected Overrides ReadOnly Property ViewName() As String
-			Get
-				Return MyGridViewName
-			End Get
-		End Property
+        Public Sub New()
+            MyBase.New()
+        End Sub
+        Public Sub New(ByVal grid As GridControl)
+            MyBase.New(grid)
+        End Sub
 
-		Protected Overrides Function CreateColumnCollection() As GridColumnCollection
-			Return New MyGridColumnCollection(Me)
-		End Function
-	End Class
+        Friend Const MyGridViewName As String = "MyGridView"
 
-	Public Class MyGridViewInfoRegistrator
-		Inherits GridInfoRegistrator
-		Public Sub New()
-			MyBase.New()
-		End Sub
+        Protected Overrides ReadOnly Property ViewName() As String
+            Get
+                Return MyGridViewName
+            End Get
+        End Property
 
-		Public Overrides ReadOnly Property ViewName() As String
-			Get
-				Return MyGridView.MyGridViewName
-			End Get
-		End Property
+        Protected Overrides Function CreateColumnCollection() As GridColumnCollection
+            Return New MyGridColumnCollection(Me)
+        End Function
+    End Class
 
-		Public Overrides Function CreateView(ByVal grid As GridControl) As BaseView
-			Return New MyGridView(grid)
-		End Function
-	End Class
+    Public Class MyGridViewInfoRegistrator
+        Inherits GridInfoRegistrator
 
-	Public Class MyGridColumnCollection
-		Inherits GridColumnCollection
-		Public Sub New(ByVal view As ColumnView)
-			MyBase.New(view)
-		End Sub
+        Public Sub New()
+            MyBase.New()
+        End Sub
 
-		Protected Overrides Function CreateColumn() As GridColumn
-			Return New MyGridColumn()
-		End Function
-	End Class
+        Public Overrides ReadOnly Property ViewName() As String
+            Get
+                Return MyGridView.MyGridViewName
+            End Get
+        End Property
 
-	Public Class MyGridColumn
-		Inherits GridColumn
-		Public Sub New()
-			MyBase.New()
-		End Sub
+        Public Overrides Function CreateView(ByVal grid As GridControl) As BaseView
+            Return New MyGridView(grid)
+        End Function
+    End Class
 
-		Private oldVisibleIndex_Renamed As Integer = -1
-		<XtraSerializableProperty, XtraSerializablePropertyId(LayoutIdLayout)> _
-		Public Property OldVisibleIndex() As Integer
-			Get
-				Return oldVisibleIndex_Renamed
-			End Get
-			Set(ByVal value As Integer)
-				If value = -1 Then
-					Return
-				End If
-				oldVisibleIndex_Renamed = value
-			End Set
-		End Property
+    Public Class MyGridColumnCollection
+        Inherits GridColumnCollection
 
-		Protected Overrides Sub Assign(ByVal column As GridColumn)
-			MyBase.Assign(column)
-			Dim source As MyGridColumn = TryCast(column, MyGridColumn)
-			If source Is Nothing Then
-				Return
-			End If
-			OldVisibleIndex = source.OldVisibleIndex
-		End Sub
+        Public Sub New(ByVal view As ColumnView)
+            MyBase.New(view)
+        End Sub
 
-		Public Overrides Property VisibleIndex() As Integer
-			Get
-				Return MyBase.VisibleIndex
-			End Get
-			Set(ByVal value As Integer)
-				oldVisibleIndex_Renamed = value
-				MyBase.VisibleIndex = value
-			End Set
-		End Property
+        Protected Overrides Function CreateColumn() As GridColumn
+            Return New MyGridColumn()
+        End Function
+    End Class
 
-		Public Overrides Property Visible() As Boolean
-			Get
-				Return MyBase.Visible
-			End Get
-			Set(ByVal value As Boolean)
-				If (Not MyBase.Visible) AndAlso value AndAlso VisibleIndex = -1 Then
-					VisibleIndex = OldVisibleIndex
-				End If
-				MyBase.Visible = value
-			End Set
-		End Property
-	End Class
+    Public Class MyGridColumn
+        Inherits GridColumn
+
+        Public Sub New()
+            MyBase.New()
+        End Sub
+
+
+        Private oldVisibleIndex_Renamed As Integer = -1
+        <XtraSerializableProperty, XtraSerializablePropertyId(LayoutIdLayout)> _
+        Public Property OldVisibleIndex() As Integer
+            Get
+                Return oldVisibleIndex_Renamed
+            End Get
+            Set(ByVal value As Integer)
+                If value = -1 Then
+                    Return
+                End If
+                oldVisibleIndex_Renamed = value
+            End Set
+        End Property
+
+        Protected Overrides Sub Assign(ByVal column As GridColumn)
+            MyBase.Assign(column)
+            Dim source As MyGridColumn = TryCast(column, MyGridColumn)
+            If source Is Nothing Then
+                Return
+            End If
+            OldVisibleIndex = source.OldVisibleIndex
+        End Sub
+
+        Public Overrides Property VisibleIndex() As Integer
+            Get
+                Return MyBase.VisibleIndex
+            End Get
+            Set(ByVal value As Integer)
+                oldVisibleIndex_Renamed = value
+                MyBase.VisibleIndex = value
+            End Set
+        End Property
+
+        Public Overrides Property Visible() As Boolean
+            Get
+                Return MyBase.Visible
+            End Get
+            Set(ByVal value As Boolean)
+                If Not MyBase.Visible AndAlso value AndAlso VisibleIndex = -1 Then
+                    VisibleIndex = OldVisibleIndex
+                End If
+                MyBase.Visible = value
+            End Set
+        End Property
+    End Class
 End Namespace
